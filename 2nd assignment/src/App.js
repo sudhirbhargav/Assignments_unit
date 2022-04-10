da4dabaaee594c216e60a380c765259eb5bdb215
Sudhir Bhargav <93834423+sudhirbhargav@users.noreply.github.com>
2022-04-11 01:33:10 +0530

    Add files via upload

---
+++ 2nd assignment/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Textform from './compo/Textform';
+
+function App() {
+  return (
+    <>
+    <Textform></Textform>
+    </>
+  );
+}
+
+export default App;
---
+++ 2nd assignment/src/compo/Textform.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+if(count%2==0){
+    <style>
+        h1{
+            colour
+        }
+    </style>
+}
+
+export default function Textform() {
+    const [count,setCount] = useState(0)
+    let inc = (num) =>{
+        setCount(count + num)
+    }
+    let dic = (num) =>{
+        setCount(count - num)
+    }
+    let double = (num) =>{
+        setCount(count * num)
+    }
+  return (
+    <>
+    <h1>{count}</h1>
+    <button onClick={()=>inc(1)}>Inc</button>
+    <button onClick={()=>dic(1)}>dic</button>
+    <button onClick={()=>double(2)}>Double</button>
+    </>
+  )
+}
